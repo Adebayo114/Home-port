159689e29e34f3529738462cdfe347241156e843
Adebayo Oladayo <matthewadebayo392@gmail.com>
2023-11-24 18:54:09 +0100

    move

---
+++ index.js
@@ -0,0 +1,4 @@
+       // JavaScript to toggle the dropdown on click
+       document.querySelector('.sub-main').addEventListener('click', function () {
+        document.querySelector('.dropdown').classList.toggle('active');
+    });
