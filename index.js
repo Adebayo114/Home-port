2b368b8ff11b50a7393a6c190f759014c87e2f9f
Adebayo Oladayo <matthewadebayo392@gmail.com>
2023-11-26 02:18:37 +0100

    move

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-       // JavaScript to toggle the dropdown on click
-       document.querySelector('.sub-main').addEventListener('click', function () {
-        document.querySelector('.dropdown').classList.toggle('active');
-    });
+const menu = document.querySelector(".icon");
+const menu1 = document.querySelector("menu")
+const close1 =document.querySelector(".close")
+const dts0 = document.getElementById( "dts0" )
+const dts1 = document.getElementById( "dts1" )
+const dts2 = document.getElementById( "dts2" )
+const dts3 = document.getElementById( "dts3" )
+const dts4 = document.getElementById( "dts4" )
+
+menu.addEventListener("click", function(){
+    menu1.classList.add("active");
+    // console.log("i am working");
+});
+
+close1.addEventListener("click" , function(){
+    menu1.classList.remove("active");
+})
+
+dts0.addEventListener(`click`, function(){
+    menu1.classList.remove("active");
+})
+
+dts1.addEventListener(`click`, function(){
+    menu1.classList.remove("active");
+})
+
+dts2.addEventListener(`click`, function(){
+    menu1.classList.remove("active");
+})
+
+dts3.addEventListener(`click`, function(){
+    menu1.classList.remove("active");
+})
+
+dts4.addEventListener(`click`, function(){
+    menu1.classList.remove("active");
+})
+
+
+
+
